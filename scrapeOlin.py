713aa796f715207ea9d81ba3e8b587894b90a9ad
teadetime <ntfaber@gmail.com>
2020-04-05 21:55:19 -0700

    Started bs4 parsing, navigates down to the course level!

--- scrapeOlin.py
+++ scrapeOlin.py
@@ -9,17 +9,47 @@ from pickle import dump, load
 # Create a directory to store the Lrics in
 
 # Set up Scraping links for Beautiful Soup
-root_link = "https://olin.smartcatalogiq.com/en/2019-20/Catalog/Courses-Credits-Hours"
+base_link = "https://olin.smartcatalogiq.com/"
+root_link = base_link + "en/2019-20/Catalog/Courses-Credits-Hours"
 
-    # Extract the contents of the link
-    link_page = requests.get(root_link)
+# Extract the contents of the link
+link_page = requests.get(root_link)
+soup = bs(link_page.content, 'html.parser')
+groups = soup.find_all('a', attrs={"href": re.compile('Catalog/Courses-Credits-Hours/')})  #
+print(groups)
+# Now lets put those urls into a dictionary
+groupDict = []
+for aGroup in groups:
+    groupUrl = aGroup.get('href')
+    group_name = aGroup.getText()
+    # print(group_name)
+    # print(groupUrl)
+    # There are duplicate links so don't duplicate entries
+    if group_name not in groupDict:
+        groupDict.append({'url': groupUrl})
+print(groupDict)
+
+
+#### Now scrape each of the links
+for group in groupDict:
+    print(group)
+    print(type(group))
+    url = group['url']
+    group_link = base_link + url
+    print(group_link)
+
+    # Get links for each page and store them in a list
+    link_page = requests.get(group_link)
     soup = bs(link_page.content, 'html.parser')
-    aTagGroup = soup.find_all('a', attrs={"href": re.compile(^'https://olin.smartcatalogiq.com/en/2019-20/Catalog/Courses-Credits-Hours/')})  #
-    '''
-    # Now lets put those urls into a dictionary
-    songDict = {}
-    for aTag in aTagGroup:
-        songUrl = aTag.get('href')
-        # There are duplicate songnames so don't duplicate entries
-        if songName not in songDict:
-            songDict[songName] = {'url': songUrl}
+
+    right_pnl = soup.find_all('div', attrs={"id": 'rightpanel'})  #
+    classes = right_pnl[0].find_all('a', attrs={"href": re.compile(url+'/')})  #
+    for course in classes:
+        # PARSE THE COURSE PAGE!!!!
+        print(course)
+        course_link = base_link + course.get('href')
+        link_page = requests.get(course_link)
+        soup = bs(link_page.content, 'html.parser')
+        credits = soup.find_all('div', attrs={"class": 'credits'})  #
+        print(credits[0].getText())
+    break
