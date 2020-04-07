b70a8550a384833784dfe1d118c3a8dbbdd01aea
teadetime <ntfaber@gmail.com>
2020-04-07 16:57:20 -0700

    made sample coad a lot simpler, added recursion limit added __main__

--- editCatalog.py
+++ editCatalog.py
@@ -1,31 +1,6 @@
-def pickle_data(file_path, data, overwrite):
-    """
-    Pickle a specific piece of data in binary. Can be pickled at any directory, will not overwrite current data unless
-        overwite is specified
-    :param file_path: Npath to file to be pickled. Can contain just a file name to pickle in CWD
-    :param data: The data that will be pickled
-    :param overwrite: Boolean that indicates whether the file should be overwritten even if it exists
-    :return: #TODO Nothing currently should return True if succesful
-    """
-    if not os.path.exists(file_path) or overwrite:
-        f = open(file_path, "wb")
-        dump(data, f)
-        f.close()
+import os
+from scrapeOlin import pickle_data, load_pickle_data
 
-
-def load_pickle_data(file_path):
-    """
-    Unpickles data stored at a given file path (or filename in CWD) and returns it
-    :param file_path: path to file that is to be un-pickled (can be file name for items in CWD)
-    :return: Returns None or that data that was stored in pickle form at the location
-    """
-    # Initialize data so that None is returned if file path doesn't work
-    pickled_data = None
-    if os.path.exists(file_path):
-        f = open(file_path, "rb+")
-        pickled_data = load(f)
-        f.close()
-    return pickled_data
-
-catalog = load_pickle_data('catalog_picle')
+catalog = load_pickle_data('catalog_pickle')
 print(catalog)
+print(len(catalog.items()))
--- scrapeOlin.py
+++ scrapeOlin.py
@@ -8,9 +8,6 @@ import re
 from pickle import dump, load
 import sys
 
-print(sys.getrecursionlimit())
-sys.setrecursionlimit(10**6)
-print(sys.getrecursionlimit())
 
 def pickle_data(file_path, data, overwrite):
     """
@@ -21,6 +18,7 @@ def pickle_data(file_path, data, overwrite):
     :param overwrite: Boolean that indicates whether the file should be overwritten even if it exists
     :return: #TODO Nothing currently should return True if succesful
     """
+    sys.setrecursionlimit(10 ** 6)
     if not os.path.exists(file_path) or overwrite:
         f = open(file_path, "wb")
         dump(data, f)
@@ -156,77 +154,78 @@ def parse_info(info_div):
     info = info_div[0].getText()
     return info
 
+if __name__ == '__main__':
 
-# Set up Scraping links for Beautiful Soup
-base_link = "https://olin.smartcatalogiq.com/"
-root_link = base_link + "en/2019-20/Catalog/Courses-Credits-Hours"
-# Extract the contents of the link
-link_page = requests.get(root_link)
-soup = bs(link_page.content, 'html.parser')
-right_pnl = soup.find_all('div', attrs={"id": 'rightpanel'})
-groups = right_pnl[0].find_all('a', attrs={"href": re.compile('Catalog/Courses-Credits-Hours/')})  #
-print(groups)
-# Now lets put those urls into a dictionary
-groupDict = []
-for aGroup in groups:
-    groupUrl = aGroup.get('href')
-    group_name = aGroup.getText()
-    # print(group_name)
-    # print(groupUrl)
-    # There are duplicate links so don't duplicate entries
-    if group_name not in groupDict:
-        groupDict.append({'url': groupUrl})
-print(groupDict)
-
-#### Now scrape each of the links
-catalog = {}
-for group in groupDict:
-    print(group)
-    print(type(group))
-    url = group['url']
-    group_link = base_link + url
-    print(group_link)
-
-    # Get links for each page and store them in a list
-    link_page = requests.get(group_link)
+    # Set up Scraping links for Beautiful Soup
+    base_link = "https://olin.smartcatalogiq.com/"
+    root_link = base_link + "en/2019-20/Catalog/Courses-Credits-Hours"
+    # Extract the contents of the link
+    link_page = requests.get(root_link)
     soup = bs(link_page.content, 'html.parser')
+    right_pnl = soup.find_all('div', attrs={"id": 'rightpanel'})
+    groups = right_pnl[0].find_all('a', attrs={"href": re.compile('Catalog/Courses-Credits-Hours/')})  #
+    print(groups)
+    # Now lets put those urls into a dictionary
+    groupDict = []
+    for aGroup in groups:
+        groupUrl = aGroup.get('href')
+        group_name = aGroup.getText()
+        # print(group_name)
+        # print(groupUrl)
+        # There are duplicate links so don't duplicate entries
+        if group_name not in groupDict:
+            groupDict.append({'url': groupUrl})
+    print(groupDict)
 
-    right_pnl = soup.find_all('div', attrs={"id": 'rightpanel'})  #
-    classes = right_pnl[0].find_all('a', attrs={"href": re.compile(url + '/')})  #
-    for course in classes:
-        # PARSE THE COURSE PAGE!!!!
-        course_link = base_link + course.get('href')
-        #course_link = "https://olin.smartcatalogiq.com/en/2019-20/Catalog/Courses-Credits-Hours/OIE-Olin-Intro-Experience/1000/OIE1000" # Test an individual site!
-        crn = course_link.split('/')[-1]
-        print(crn)
-        link_page = requests.get(course_link)
+    #### Now scrape each of the links
+    catalog = {}
+    for group in groupDict:
+        print(group)
+        print(type(group))
+        url = group['url']
+        group_link = base_link + url
+        print(group_link)
+
+        # Get links for each page and store them in a list
+        link_page = requests.get(group_link)
         soup = bs(link_page.content, 'html.parser')
-        reqs_pre = soup.find_all('div', attrs={"class": 'sc-preReqs'})
-        credits = soup.find_all('div', attrs={"class": 'credits'})
-        reqs_co = soup.find_all('div', attrs={"class": 'sc-coReqs'})
-        reqs_con = soup.find_all('div', attrs={"class": 'sc-concurrentReqs'})
-        reqs_rec = soup.find_all('div', attrs={"class": 'sc-recommendedReqs'})
-        hours_div = soup.find_all('div', attrs={"class": 'sc-Attributes'})
-        info_div = soup.find_all('div', attrs={"class": 'desc'})
 
-        # print(reqs_pre[0].contents[-1]) # Use contents to get the last child!
+        right_pnl = soup.find_all('div', attrs={"id": 'rightpanel'})  #
+        classes = right_pnl[0].find_all('a', attrs={"href": re.compile(url + '/')})  #
+        for course in classes:
+            # PARSE THE COURSE PAGE!!!!
+            course_link = base_link + course.get('href')
+            #course_link = "https://olin.smartcatalogiq.com/en/2019-20/Catalog/Courses-Credits-Hours/OIE-Olin-Intro-Experience/1000/OIE1000" # Test an individual site!
+            crn = course_link.split('/')[-1]
+            print(crn)
+            link_page = requests.get(course_link)
+            soup = bs(link_page.content, 'html.parser')
+            reqs_pre = soup.find_all('div', attrs={"class": 'sc-preReqs'})
+            credits = soup.find_all('div', attrs={"class": 'credits'})
+            reqs_co = soup.find_all('div', attrs={"class": 'sc-coReqs'})
+            reqs_con = soup.find_all('div', attrs={"class": 'sc-concurrentReqs'})
+            reqs_rec = soup.find_all('div', attrs={"class": 'sc-recommendedReqs'})
+            hours_div = soup.find_all('div', attrs={"class": 'sc-Attributes'})
+            info_div = soup.find_all('div', attrs={"class": 'desc'})
 
-        reqs_pre_list = parse_req(reqs_pre)
-        reqs_co_list = parse_req(reqs_co)
-        reqs_con_list = parse_req(reqs_con)
-        reqs_rec_list = parse_req(reqs_rec)
-        '''
-        print("Pre: ", reqs_pre_list)
-        print("Co: ", reqs_co_list)
-        print("Con: ", reqs_con_list)
-        print("Recc: ", reqs_rec_list)
-        '''
-        credit_dict = parse_cred(credits)
-        #print(credit_dict)
-        info = parse_info(info_div)
-        hours_dict = parse_hrs(hours_div, True)
-        #print(hours_dict)
-        catalog[crn] = course_dict(crn, reqs_pre_list, reqs_co_list, reqs_con_list, reqs_rec_list, info, credit_dict,
-                                   hours_dict)
-        sleep(.2)
-pickle_data('catalog_pickle', catalog, True)
+            # print(reqs_pre[0].contents[-1]) # Use contents to get the last child!
+
+            reqs_pre_list = parse_req(reqs_pre)
+            reqs_co_list = parse_req(reqs_co)
+            reqs_con_list = parse_req(reqs_con)
+            reqs_rec_list = parse_req(reqs_rec)
+            '''
+            print("Pre: ", reqs_pre_list)
+            print("Co: ", reqs_co_list)
+            print("Con: ", reqs_con_list)
+            print("Recc: ", reqs_rec_list)
+            '''
+            credit_dict = parse_cred(credits)
+            #print(credit_dict)
+            info = parse_info(info_div)
+            hours_dict = parse_hrs(hours_div, True)
+            #print(hours_dict)
+            catalog[crn] = course_dict(crn, reqs_pre_list, reqs_co_list, reqs_con_list, reqs_rec_list, info, credit_dict,
+                                       hours_dict)
+            sleep(.2)
+    pickle_data('catalog_pickle', catalog, True)
