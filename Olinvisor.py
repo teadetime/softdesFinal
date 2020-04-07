c4e06815305a01841ac7dbdeb517c362572b58de
adi r <aramachandran@olin.edu>
2020-04-07 19:20:19 -0400

    began building out core scheduling algorithm functionality, more work needed in helper functions, handling optional interchangable courses, & final layer rescheduling

--- Olinvisor.py
+++ Olinvisor.py
@@ -24,7 +24,7 @@ class Course:
     def __init__(self):
     # Maybe there should be a list of credit types, that are passes
         self.crn =
-        self.pre_req =
+        self.pre_req =              # store prereqs in a list
         self.mth_crds =
         self.sci_crds =
         self.ahs_crds =
@@ -49,44 +49,100 @@ class Major:
             self.one_reqs.append(group) # list of lists for one required Courses
 
 
+"""
+Techincal questions:
+how to represent interchangable course options (tuples?)
+how to represent course options
+work from top down or bottom up?
+
+start with absolute courses, work top down
+
+how to represent LOA's?
+How to optimize for different weight_preferences?
+
+"""
+
+# Adi's assumption that there is a hashmap where you can access a Course object from its CRN
+allCoursesKeys = {}
+
 class Schedule:
     """
     This class represents your entrire stay at olin aka 10 semesters max?
     can be stored locally to save progress or specific plans that are generated"""
-    # TODO: fill sem1_classes default constructor below
-    def __init__(self, major, weight_preferences, sem2_classes,sem1_classes=[], BOW_classes=None, LOA_sems=None): # include *args or **kwargs to account for external factors, BOW classes, etc.
-        # Initialize Empty Schedule
+    # TODO: fill courses taken default constructor with sem 1 & sem 2 courses below somehow
+    def __init__(self, major, weight_preferences, courses_taken=[], BOW_classes=None, LOA_sems=None): # include *args or **kwargs to account for external factors, BOW classes, etc.
+        # Initialize Empty Schedule with built in LOA's, courses_taken
+        # things to note: courses taken is list of lists,
         self.major = Major
         self.name = Major.name
-        self.sem1 = sem1_classes
-        self.sem2 = sem2_classes
-        self.sem9 = []
-        self.sem3 = []
-        self.sem4 = []
-        self.sem5 = []
-        self.sem5 = []
-        self.sem7 = []
-        self.sem8 = []
-        self.sem10 = []
-        self.course_progression = [self.sem1, self.sem2, self.sem3, self.sem4, self.sem5, self.sem6, self.sem7, self.sem8, self.sem9, self.sem10]
+        self.course_progression = []
+        self.course_progression = self.course_progression.extend(courses_taken)
+        for i in range(len(10-len(self.course_progression))): # TODO: numbers might be off
+            course_progression.append([])
+
+        if LOA_sems is not None:
+            for semester in LOA_sems:
+                self.course_progression[semester] = ['LOACRN'] # TODO: placeholder - We need to add a CRN for LOA's etc.
         self.weight_preferences = weight_preferences
 
-    def insertCourse(self,course):
-        findOpening(course,
 
-    def findOpening(self, course):
-        for i in range(len(self.course_progression)):
-            if self.course_progression[i]
+    # begin helper function definitions for primary forge_schedule function
+    def insertAbsCoursesHelper(self,courseList ):
+        """
+        top down - find highest course (w/ most prereqs) in absolute list, break down prereqs, insert recursively at top as needed
+        """
+        for course in reveresed(courseList):
+            self.insertCoursesRec(course, self.findTopSlot(course))
 
-    def checkPrereqs(self, course, position):
+    def insertOptCoursesHelper(self):
+        """
+        TODO: For the MVP this groups the interchangable courses that require the same number of prereqs and inserts them into the course structure.  
+        """
 
 
+    def insertCoursesRec(self, course, slot): # course to insert, slot is an index
+        # conditional to check for prereqs
+        """
+        TODO: Do we want to use findTopSlot() in the function call of insertCoursesRec or do we want to use it here?
+        related TODO: Should findTopSlot() take in a top possibility? That would make it more versatile.
+        """
+        if course.pre_req is not None:
+            for prereq in course.pre_req: # walk through list
+                self.insertCoursesRec(prereq, slot-1)
+        self.course_progression[slot].append(course)
 
-    def forge_schedule(self):
-        for course in self.major.abs_reqs:
-            self.insertCourse(course)
-        for course in self.major.one_reqs:
-            self.insertCourse
+
+    def findTopSlot(self, testCourse):
+        """A lot of code to do something very simple: find the highest possible slot to insert a course"""
+        temp = len(self.course_progression)
+        for semester in reversed(self.course_progression):
+            temp -= 1
+            semesterCredits = 0
+            if len(semester) is not None:
+                for course in semester:
+                    semesterCredits+=course.credits
+            if self.weight_preferences - semesterCredits > testCourse.credits:
+                return(temp)
+
+    def reorganizeCoures(self):
+        # TODO: schedule reorganizaiton functionality
+        pass
+
+
+    def forgeSchedule(self):
+        """Walks through all required courses in a major starting with abs_reqs, then one_reqs,
+        and fills out schedule from end to start.  """
+        self.insertAbsCoursesHelper(self.major.abs_reqs)
+        # for optional_course_list in self.major.one_reqs:
+        self.insertOptCoursesHelper()
+
+
+        # rebuild schedule based
+        # Alternatively - define intermediary data structure to store course progression before this reorganizaiton step
+        self.reorganizeCoures()
+        return self.course_progression
+
+
 
 
         return self.course_progression
